31ab6185157c9c5283daf1044d4c682b4b8439c9
Josue Cortez <josue.cortez222@gmail.com>
2022-04-25 17:26:54 -0600

    ADD: pruebas para las clases Evaluacion y Programas.

--- sms-solidProject/src/main/java/csf6/grupo04/Main.java
+++ sms-solidProject/src/main/java/csf6/grupo04/Main.java
@@ -1,7 +1,12 @@
 package csf6.grupo04;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido!");
+
+        Evaluacion eval = new Evaluacion("Proyecto SOLID", "Programacion", 10, "Falta aplicar principios SOLID");
+        System.out.println(eval.listarEvaluaciones());
+
+        Evaluacion program = new Programas("Proyecto SOLID", "Programacion", 10, "Falta aplicar principios SOLID", "ruta del archivo :P", 1, "Nada importante");
+        System.out.println(program.listarEvaluaciones());
     }
 }
