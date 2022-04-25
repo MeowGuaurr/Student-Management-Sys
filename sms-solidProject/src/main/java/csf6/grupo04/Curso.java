fea03a4a6b460925cdc3151639db366e985cce49
MeowGuaurr <reynitamercedes@gmail.com>
2022-04-25 00:07:38 -0600

    clase Persona y Curso creada, pendiente actualización de clase Coach para integrarlo

---
+++ sms-solidProject/src/main/java/csf6/grupo04/Curso.java
@@ -0,0 +1,20 @@
+package csf6.grupo04;
+
+public class Curso {
+    private String nombre;
+    private String codigo;
+    private int annio;
+    private int vacantes;
+    private int inscritos;
+    private boolean finalizado;
+    //pendiente atributo coach
+
+    public Curso() {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.annio = annio;
+        this.vacantes = vacantes;
+        this.inscritos = inscritos;
+    }
+    //pendiente métodos
+}
---
+++ sms-solidProject/src/main/java/csf6/grupo04/Persona.java
@@ -0,0 +1,25 @@
+package csf6.grupo04;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String email;
+    private int telefono;
+
+    public Persona(String valornombre, String valorapellido, int valoredad, String valoremail, int valortelefono)
+    {
+        nombre = valornombre;
+        apellido = valorapellido;
+        edad = valoredad;
+        email = valoremail;
+        telefono = valortelefono;
+    }
+
+    public String listarPersonas()
+    {
+        //falta definir el retorno del método
+    }
+}
+
+
