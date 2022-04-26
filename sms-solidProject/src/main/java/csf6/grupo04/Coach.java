7143b2032b1bc086082baf54f11d03e9a8c8a8e2
Josue Cortez <josue.cortez222@gmail.com>
2022-04-26 16:45:23 -0600

    ADD: clase Coach y su herencia de Personas.

---
+++ sms-solidProject/src/main/java/csf6/grupo04/Coach.java
@@ -0,0 +1,51 @@
+package csf6.grupo04;
+
+public class Coach extends Persona{
+    // Declaracion de variables
+    private int identificacion;
+    private String direccion;
+
+    // Getters y Setters
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    // Constructor
+    public Coach() {}
+
+    public Coach(String nombre, String apellido, int edad, String email, int telefono, int identificacion, String direccion){
+        super();
+        setNombre(nombre);
+        setApellido(apellido);
+        setEdad(edad);
+        setEmail(email);
+        setTelefono(telefono);
+        this.identificacion = identificacion;
+        this.direccion = direccion;
+    }
+
+    // Método para listar Coach
+    public String listarCoach(){
+        return "Identifiación: " + getIdentificacion() +
+                "Nombre: " + getNombre() + " " + getApellido() +
+                "\nEdad: " + getEdad() +
+                "\nE-mail: " + getEmail() +
+                "\nTeléfono:" + getTelefono() +
+                "\nDirección: " + getDireccion() +
+                "\n----------------------------------------";
+        //implementar añadir al .csv
+    }
+}
