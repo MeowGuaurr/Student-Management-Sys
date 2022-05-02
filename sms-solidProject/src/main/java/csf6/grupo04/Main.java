8594fd6d9b412cb2927fcc70ba695684da383a08
Josue Cortez <josue.cortez222@gmail.com>
2022-05-01 23:34:12 -0600

    MOD: se modifico el Main para que se repita el menu.

--- sms-solidProject/src/main/java/csf6/grupo04/Main.java
+++ sms-solidProject/src/main/java/csf6/grupo04/Main.java
@@ -7,6 +7,72 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
+        int repetir = 1;
+
+        while (repetir == 1){
+            switch (menu()) {
+                case 1 -> ControllerEstudiante.verEstudiantes();
+                case 2 -> ControllerCoach.verCoaches();
+                case 3 -> ControllerAdministrativo.verAdmins();
+                case 4 -> {
+                    int accionRegistro;
+                    imprimirLN("""
+                Que quiere registrar:
+                01. Registrar estudiante.
+                02. Registrar coach.
+                03. Registrar personal administrativo.""");
+                    imprimir("\nDigite el número: ");
+                    accionRegistro = lector.nextInt();
+
+                    while (accionRegistro < 1 || accionRegistro > 3){
+                        imprimir("--------------------------------------------");
+                        imprimirLN("Debe seleccionar un número de valido!");
+                        imprimir("\nDigite el número: ");
+                        accionRegistro = lector.nextInt();
+                        imprimir("--------------------------------------------\n");
+                    }
+
+                    switch (accionRegistro){
+                        case 1 -> ControllerEstudiante.registrarEstudiante();
+                        case 2 -> ControllerCoach.registrarCoach();
+                        case 3 -> ControllerAdministrativo.registrarAdmin();
+                    }
+                }
+                case 5 -> {
+                    imprimirLN("Modificación de datos de persona:");
+                    imprimirLN("En proceso....");
+                }
+                case 6 -> ControllerCurso.verCursos();
+                case 7 -> ControllerCurso.registrarCurso();
+                case 8 -> {
+                    imprimirLN("Modificación de curso:");
+                    imprimirLN("En proceso....");
+                }
+                case 9 -> ControllerEvaluacion.registrarEvaluacion();
+                case 10 -> ControllerEvaluacion.verEvaluaciones();
+            }
+
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            imprimirLN("""
+                    Desea realizar otra acción:" +
+                    1. Si.
+                    2. No.
+                    """);
+            repetir = lector.nextInt();
+        }
+    }
+
+    // Métodos para mostrar en pantalla
+    public static void imprimir(String texto){
+        System.out.println(texto);
+    }
+    public static void imprimirLN(String texto){
+        System.out.println(texto);
+    }
+
+    public static int menu(){
+        Scanner lector = new Scanner(System.in);
         int accion;
 
         imprimirLN("Bienvenido al Sistema de manejo de estudiantes!");
@@ -21,14 +87,13 @@ public class Main {
                 07. Añadir un curso nuevo.
                 08. Modificar un curso.
                 09. Registrar una evaluación.
-                10. Listar las evaluaciones.
-                11. Abrir carpeta contenedora de trabajos.""");
+                10. Listar las evaluaciones.""");
         imprimir("\nDigite el número: ");
         accion = lector.nextInt();
 
         imprimirLN("");
 
-        while (accion < 1 || accion > 11){
+        while (accion < 1 || accion > 10){
             imprimir("--------------------------------------------");
             imprimirLN("Debe seleccionar un número de valido!");
             imprimir("\nDigite el número: ");
@@ -36,59 +101,6 @@ public class Main {
             imprimir("--------------------------------------------\n");
         }
 
-        switch (accion) {
-            case 1 -> ControllerEstudiante.verEstudiantes();
-            case 2 -> ControllerCoach.verCoaches();
-            case 3 -> ControllerAdministrativo.verAdmins();
-            case 4 -> {
-                int accionRegistro;
-                imprimirLN("""
-                Que quiere registrar:
-                01. Registrar estudiante.
-                02. Registrar coach.
-                03. Registrar personal administrativo.""");
-                imprimir("\nDigite el número: ");
-                accionRegistro = lector.nextInt();
-
-                while (accionRegistro < 1 || accionRegistro > 3){
-                    imprimir("--------------------------------------------");
-                    imprimirLN("Debe seleccionar un número de valido!");
-                    imprimir("\nDigite el número: ");
-                    accionRegistro = lector.nextInt();
-                    imprimir("--------------------------------------------\n");
-                }
-
-                switch (accionRegistro){
-                    case 1 -> ControllerEstudiante.registrarEstudiante();
-                    case 2 -> ControllerCoach.registrarCoach();
-                    case 3 -> ControllerAdministrativo.registrarAdmin();
-                }
-            }
-            case 5 -> {
-                imprimirLN("Modificación de datos de persona:");
-                imprimirLN("En proceso....");
-            }
-            case 6 -> ControllerCurso.verCursos();
-            case 7 -> ControllerCurso.registrarCurso();
-            case 8 -> {
-                imprimirLN("Modificación de curso:");
-                imprimirLN("En proceso....");
-            }
-            case 9 -> ControllerEvaluacion.registrarEvaluacion();
-            case 10 -> ControllerEvaluacion.verEvaluaciones();
-            case 11 -> {
-                imprimirLN("Abrir carpeta contenedora de archivos.");
-                imprimirLN("En proceso....");
-            }
-        }
-
-    }
-
-    // Métodos para mostrar en pantalla
-    public static void imprimir(String texto){
-        System.out.println(texto);
-    }
-    public static void imprimirLN(String texto){
-        System.out.println(texto);
+        return accion;
     }
 }
