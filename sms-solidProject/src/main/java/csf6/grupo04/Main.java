829062c8a3c06482d27d337cc7474f556d50cdbb
Josue Cortez <josue.cortez222@gmail.com>
2022-05-11 16:40:25 -0600

    MOD: se modifico el menu.

--- sms-solidProject/src/main/java/csf6/grupo04/Main.java
+++ sms-solidProject/src/main/java/csf6/grupo04/Main.java
@@ -19,77 +19,17 @@ public class Main {
                 case 1 -> ControllerEstudiante.verEstudiantes();
                 case 2 -> ControllerCoach.verCoaches();
                 case 3 -> ControllerAdministrativo.verAdmins();
-                case 4 -> {
-                    imprimirLN("""
-                Que quiere registrar:
-                01. Registrar estudiante.
-                02. Registrar coach.
-                03. Registrar personal administrativo.""");
-                    imprimir("\nDigite el número: ");
-                    accionRegistro = lector.nextInt();
-
-                    while (accionRegistro < 1 || accionRegistro > 3){
-                        imprimir("--------------------------------------------");
-                        imprimirLN("Debe seleccionar un número de valido!");
-                        imprimir("\nDigite el número: ");
-                        accionRegistro = lector.nextInt();
-                        imprimir("--------------------------------------------\n");
-                    }
-
-                    switch (accionRegistro){
-                        case 1 -> ControllerEstudiante.registrarEstudiante();
-                        case 2 -> ControllerCoach.registrarCoach();
-                        case 3 -> ControllerAdministrativo.registrarAdmin();
-                    }
-                }
-                case 5 -> {
-                    imprimirLN("""
-                Que quiere buscar:
-                01. Buscar estudiante.
-                02. Buscar coach.
-                03. Buscar personal administrativo.""");
-                    imprimir("\nDigite el número: ");
-                    accionBusqueda = lector.nextInt();
-
-                    while (accionBusqueda < 1 || accionBusqueda > 3){
-                        imprimir("--------------------------------------------");
-                        imprimirLN("Debe seleccionar un número de valido!");
-                        imprimir("\nDigite el número: ");
-                        accionBusqueda = lector.nextInt();
-                        imprimir("--------------------------------------------\n");
-                    }
-
-                    switch (accionBusqueda){
-                        case 1 -> ControllerEstudiante.buscarEstudiante();
-                        case 2 -> ControllerCoach.buscarCoach();
-                        case 3 -> ControllerAdministrativo.buscarAdmin();
-                    }
-                }
-                case 6 -> ControllerCurso.verCursos();
-                case 7 -> ControllerCurso.registrarCurso();
-                case 8 -> ControllerCurso.buscarCursos();
-                case 9 -> {
-                    imprimirLN("""
-                Tipo de evaluacion:
-                01. Evaluacion normal.
-                02. Evaluacion tipo programa (Entrega de software).""");
-                    imprimir("\nDigite el número: ");
-                    accionEvaluacion = lector.nextInt();
-
-                    while (accionEvaluacion < 1 || accionEvaluacion > 2){
-                        imprimir("--------------------------------------------");
-                        imprimirLN("Debe seleccionar un número de valido!");
-                        imprimir("\nDigite el número: ");
-                        accionEvaluacion = lector.nextInt();
-                        imprimir("--------------------------------------------\n");
-                    }
-
-                    switch (accionEvaluacion){
-                        case 1 -> ControllerEvaluacion.registrarEvaluacion();
-                        case 2 -> ControllerEvaluacion.registrarProgramas();
-                    }
-                }
-                case 10 -> ControllerEvaluacion.verEvaluaciones();
+                case 4 -> ControllerCurso.verCursos();
+                case 5 -> ControllerEvaluacion.verEvaluaciones();
+                case 6 -> ControllerEstudiante.registrarEstudiante();
+                case 7 -> ControllerCoach.registrarCoach();
+                case 8 -> ControllerAdministrativo.registrarAdmin();
+                case 9 -> ControllerCurso.registrarCurso();
+                case 10 -> ControllerEvaluacion.registrarEvaluacion();
+                case 11 -> ControllerEstudiante.buscarEstudiante();
+                case 12 -> ControllerCoach.buscarCoach();
+                case 13 -> ControllerAdministrativo.buscarAdmin();
+                case 14 -> ControllerCurso.buscarCursos();
             }
 
             imprimirLN("""
@@ -99,6 +39,7 @@ public class Main {
                     """);
             imprimir("Digite el número: ");
             repetir = lector.nextInt();
+            imprimir("\n\n");
         }
     }
 
@@ -117,22 +58,32 @@ public class Main {
         imprimirLN("Bienvenido al Sistema de manejo de estudiantes!");
         imprimirLN("Por favor escriba el número correspondiente a la acción que desea realizar, del menú que se le presenta a continuación:");
         imprimirLN("""
-                01. Listar los estudiantes.
-                02. Listar los coaches.
-                03. Listar al personal administrativo.
-                04. Registrar una persona.
-                05. Buscar a una persona.
-                06. Listar los cursos.
-                07. Añadir un curso nuevo.
-                08. Buscar un curso.
-                09. Registrar una evaluación.
-                10. Listar las evaluaciones.""");
+                Listar:
+                    1. Listar los estudiantes.
+                    2. Listar los coaches.
+                    3. Listar al personal administrativo.
+                    4. Listar los cursos.
+                    5. Listar las evaluaciones.
+                
+                Registrar:
+                    6. Registrar estudiante.
+                    7. Registrar coach.
+                    8. Registrar personal administrativo.
+                    9. Registrar curso.
+                    10. Registrar evaluación.
+                
+                Buscar:
+                    11. Buscar estudiante.
+                    12. Buscar coach.
+                    13. Buscar personal administrativo.
+                    14. Buscar curso.
+                    """);
         imprimir("\nDigite el número: ");
         accion = lector.nextInt();
 
         imprimirLN("");
 
-        while (accion < 1 || accion > 10){
+        while (accion < 1 || accion > 15){
             imprimir("--------------------------------------------");
             imprimirLN("Debe seleccionar un número de valido!");
             imprimir("\nDigite el número: ");
